0f70e4b6775ac806959053b1386e394679192b31
naomu <muramatsu2023@gmail.com>
2024-01-29 10:56:42 +0900

    ショッピング課題提出

--- lib/shopping_app/cart.rb
+++ lib/shopping_app/cart.rb
@@ -1,6 +1,8 @@
 require_relative "item_manager"
+require_relative "ownable"
 
 class Cart
+  include Ownable
   include ItemManager
 
   def initialize(owner)
@@ -23,17 +25,13 @@ class Cart
   end
 
   def check_out
-    return if owner.wallet.balance < total_amount
-  # ## 要件
-  #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
-  #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
-  #   - カートの中身（Cart#items）が空になること。
+    return if owner.wallet.balance < total_amount#購入商品金額の合計が購入者ウォレットの金額を超えた場合処理をせずここに戻る
+      @items.each do |item|
+        owner.wallet.withdraw(item.price)#購入した商品の金額をwithdrawメソッドで所持金額から引いている
+        item.owner.wallet.deposit(item.price)#購入者が購入した商品の合計金額をdepositメソッドを使って販売者の所持金額に追加する
+        item.owner = owner#購入した商品のオーナーを販売者から購入者に移行
+      end
 
-  # ## ヒント
-  #   - カートのオーナーのウォレット ==> self.owner.wallet
-  #   - アイテムのオーナーのウォレット ==> item.owner.wallet
-  #   - お金が移されるということ ==> (？)のウォレットからその分を引き出して、(？)のウォレットにその分を入金するということ
-  #   - アイテムのオーナー権限がカートのオーナーに移されること ==> オーナーの書き換え(item.owner = ?)
+    @items = []#カートを空にする
+    end
   end
-
-end
